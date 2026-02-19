4b65df3abc3c85a6c2c0a82c28f1585264f88fa3
lorenaferr1520 <lorenaferreirarbd2@gmail.com>
2026-02-19 20:40:39 -0300

    just lines and try/expect

--- guessing.py
+++ guessing.py
@@ -1,6 +1,9 @@
 # Number Guessing Game
 from random import randint
 
+def linha():
+    print('-=-' * 30)
+niveis_config = {1: 50, 2: 100, 3: 500}
 computador = int()
 user = int()
 attempts = 0
@@ -9,31 +12,44 @@ nivel = {
     2: 100,
     3: 500   
 }
-
+# nivel select
+linha()
+nivel = int()
 while True:
-    dificuldade = int(input('''Choose the difficulty:
-            [1] EASY
-            [2] MEDIUM
-            [3] HARD    -->  '''))
+    try:
+        dificuldade = int(input('''Choose the difficulty:
+                [1] EASY
+                [2] MEDIUM
+                [3] HARD    -->  '''))
+    except ValueError:
+        print('ERROR! only numbers! [1-2-3]')
+        continue
 
     if dificuldade == 1:
         computador = randint(1, 50)
+        nivel = 50
         break
     elif dificuldade == 2:
         computador = randint (1, 100)
+        nivel = 100
         break
     elif dificuldade == 3:
         computador = randint (1, 500)
+        nivel = 500
         break
     else:
         print('Invalid option!')
 
-    
+linha()
+# verificação
 while True:
     while True:
-        num = int(input('Guess the number: '))
-        if num > nivel[dificuldade]:
-            print(f'Enter a number less than {nivel[computador]} ')
+        try:
+            num = int(input('Guess the number: '))
+            if num >  nivel:
+                print(f'Enter a number less than {nivel} ')
+        except ValueError:
+            print("❌ Invalid input! Please type a whole number.")
         else: 
             break
     
@@ -47,3 +63,4 @@ while True:
     elif num > computador:
         print('📉 Lower! Try again.')
         attempts += 1
+linha()
